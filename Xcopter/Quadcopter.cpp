ae50d20d35288f25aeefe5acd3e639df85cad7e0
zachlite <zachlite@gmail.com>
2015-03-21 02:52:40 -0400

    work on quadcopter class skeleton

---
+++ Xcopter/Quadcopter.cpp
@@ -0,0 +1,9 @@
+//
+//  Quadcopter.cpp
+//  Xcopter
+//
+//  Created by Zach Lite on 3/21/15.
+//  Copyright (c) 2015 Isaac Patka. All rights reserved.
+//
+
+#include "Quadcopter.h"
---
+++ Xcopter/Quadcopter.h
@@ -0,0 +1,14 @@
+//
+//  Quadcopter.h
+//  Xcopter
+//
+//  Created by Zach Lite on 3/21/15.
+//  Copyright (c) 2015 Isaac Patka. All rights reserved.
+//
+
+#ifndef __Xcopter__Quadcopter__
+#define __Xcopter__Quadcopter__
+
+#include <stdio.h>
+
+#endif /* defined(__Xcopter__Quadcopter__) */
